a552afc02509b6588e0383d9c67b1125b0154776
Adam Pash <adam.pash@gmail.com>
2014-03-18 12:18:29 -0400

    Added login and push notification for testing

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,22 @@
-var doClick;
+var Cloud, createUser, doClick;
+
+Cloud = require('ti.cloud');
 
 doClick = function(e) {
-  return alert('OMG ' + $.label.text);
+  alert('OMG ' + $.label.text);
+  return createUser();
+};
+
+createUser = function() {
+  return Cloud.Users.secureCreate({
+    title: 'Sign Up Here'
+  }, function(e) {
+    if (e.success) {
+      return alert('Success:\\n' + 'accessToken: ' + e.accessToken + '\\n' + 'expiresIn: ' + e.expiresIn);
+    } else {
+      return alert('Error:\\n' + ((e.error && e.message) || JSON.stringify(e)));
+    }
+  });
 };
 
 $.index.open();
--- src/controllers/index.coffee
+++ src/controllers/index.coffee
@@ -1,4 +1,20 @@
+Cloud = require 'ti.cloud'
+
 doClick = (e) ->
     alert 'OMG ' + $.label.text
+    createUser()
+
+createUser = ->
+  Cloud.Users.secureCreate
+      title: 'Sign Up Here'
+      ,
+      (e) ->
+        if (e.success)
+            alert('Success:\\n' +
+                'accessToken: ' + e.accessToken + '\\n' +
+                'expiresIn: ' + e.expiresIn)
+        else
+            alert('Error:\\n' +
+                ((e.error && e.message) || JSON.stringify(e)))
 
 $.index.open()
