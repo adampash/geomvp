ee7899f6543b34ed41a2548f05b590b8206afe65
Adam Pash <adam.pash@gmail.com>
2014-03-18 15:19:45 -0400

    Working iOS push notifications here

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,77 @@
-var Cloud, createUser, doClick;
+var Cloud, createUser, deviceToken, login, registerWithCloud, sendTestNotification, subscribe;
 
 Cloud = require('ti.cloud');
 
-doClick = function(e) {
-  alert('OMG ' + $.label.text);
-  return createUser();
-};
+deviceToken = null;
 
 createUser = function() {
   return Cloud.Users.secureCreate({
     title: 'Sign Up Here'
   }, function(e) {
     if (e.success) {
-      return alert('Success:\\n' + 'accessToken: ' + e.accessToken + '\\n' + 'expiresIn: ' + e.expiresIn);
+      return alert('Success:\\n' + e);
     } else {
       return alert('Error:\\n' + ((e.error && e.message) || JSON.stringify(e)));
+    }
+  });
+};
+
+login = function() {
+  return Cloud.Users.secureLogin({
+    title: 'Log In Here'
+  }, function(e) {
+    if (e.success) {
+      return alert('Logged in:\n' + Cloud.accessToken);
+    } else {
+      return alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+    }
+  });
+};
+
+subscribe = function() {
+  Ti.Network.registerForPushNotifications({
+    types: [Ti.Network.NOTIFICATION_TYPE_ALERT, Ti.Network.NOTIFICATION_TYPE_BADGE, Ti.Network.NOTIFICATION_TYPE_SOUND],
+    callback: function(e) {
+      return alert('got a push notification!');
+    },
+    success: function(e) {
+      alert('that was successful');
+      deviceToken = e.deviceToken;
+      return registerWithCloud(e);
+    },
+    error: function(e) {
+      Ti.API.debug('that was an error');
+      Ti.API.debug(e);
+      return alert(e);
+    }
+  });
+  return alert('push');
+};
+
+registerWithCloud = function(e) {
+  return Cloud.PushNotifications.subscribe({
+    channel: 'test',
+    device_token: e.deviceToken,
+    type: 'ios'
+  }, function(e) {
+    if (e.success) {
+      return alert('Subscribed :' + ((e.error && e.message) || JSON.stringify(e)));
+    } else {
+      return alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+    }
+  });
+};
+
+sendTestNotification = function() {
+  return Cloud.PushNotifications.notifyTokens({
+    to_tokens: deviceToken,
+    channel: 'test',
+    payload: 'This is a test.'
+  }, function(e) {
+    if (e.success) {
+      return alert('Push notification sent');
+    } else {
+      return alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
     }
   });
 };
--- src/controllers/index.coffee
+++ src/controllers/index.coffee
@@ -1,8 +1,5 @@
 Cloud = require 'ti.cloud'
-
-doClick = (e) ->
-    alert 'OMG ' + $.label.text
-    createUser()
+deviceToken = null
 
 createUser = ->
   Cloud.Users.secureCreate
@@ -10,11 +7,64 @@ createUser = ->
       ,
       (e) ->
         if (e.success)
-            alert('Success:\\n' +
-                'accessToken: ' + e.accessToken + '\\n' +
-                'expiresIn: ' + e.expiresIn)
+            alert('Success:\\n' + e)
         else
             alert('Error:\\n' +
                 ((e.error && e.message) || JSON.stringify(e)))
+
+login = ->
+  Cloud.Users.secureLogin
+    title: 'Log In Here'
+  , (e) ->
+    if (e.success)
+        alert('Logged in:\n' + Cloud.accessToken)
+    else
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)))
+
+subscribe = ->
+  Ti.Network.registerForPushNotifications
+    types: [
+      Ti.Network.NOTIFICATION_TYPE_ALERT,
+      Ti.Network.NOTIFICATION_TYPE_BADGE,
+      Ti.Network.NOTIFICATION_TYPE_SOUND
+    ]
+    callback: (e) ->
+      alert 'got a push notification!'
+    success: (e) ->
+      alert 'that was successful'
+      deviceToken = e.deviceToken
+      registerWithCloud(e)
+    error: (e) ->
+      Ti.API.debug 'that was an error'
+      Ti.API.debug e
+      alert e
+
+  alert 'push'
+
+registerWithCloud = (e) ->
+  Cloud.PushNotifications.subscribe
+      channel: 'test'
+      device_token: e.deviceToken
+      type: 'ios'
+  , (e) ->
+      if (e.success)
+          alert('Subscribed :'+((e.error && e.message) || JSON.stringify(e)))
+      else
+          alert('Error:\n' +
+              ((e.error && e.message) || JSON.stringify(e)))
+
+
+sendTestNotification = () ->
+    # Sends an 'This is a test.' alert to specified device if its subscribed to the 'test' channel.
+    Cloud.PushNotifications.notifyTokens
+        to_tokens: deviceToken
+        channel: 'test'
+        payload: 'This is a test.'
+    , (e) ->
+      if (e.success)
+          alert('Push notification sent')
+      else
+          alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)))
 
 $.index.open()
