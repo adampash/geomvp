7f5f8a3a5ae8890fe6defce5a778e300035758b6
Adam Pash <adam.pash@gmail.com>
2014-04-02 11:34:31 -0400

    Debugging

--- app/lib/registerForPush.js
+++ app/lib/registerForPush.js
@@ -5,29 +5,25 @@ ParsePush = require('parsePush');
 PushRegistration = {
   subscribe: function() {
     if (OS_IOS) {
-      if (Ti.Network.remoteNotificationsEnabled) {
-        return Ti.API.info('notifications are already enabled');
-      } else {
-        return Ti.Network.registerForPushNotifications({
-          types: [Ti.Network.NOTIFICATION_TYPE_ALERT, Ti.Network.NOTIFICATION_TYPE_BADGE, Ti.Network.NOTIFICATION_TYPE_SOUND],
-          callback: function(e) {
-            alert(JSON.stringify(e));
-            return Ti.Media.vibrate();
-          },
-          success: (function(_this) {
-            return function(e) {
-              var deviceToken;
-              deviceToken = e.deviceToken;
-              return _this.registerWithParse(e);
-            };
-          })(this),
-          error: function(e) {
-            Ti.API.debug('that was an error');
-            Ti.API.debug(e);
-            return alert(JSON.stringify(e));
-          }
-        });
-      }
+      return Ti.Network.registerForPushNotifications({
+        types: [Ti.Network.NOTIFICATION_TYPE_ALERT, Ti.Network.NOTIFICATION_TYPE_BADGE, Ti.Network.NOTIFICATION_TYPE_SOUND],
+        callback: function(e) {
+          alert(JSON.stringify(e));
+          return Ti.Media.vibrate();
+        },
+        success: (function(_this) {
+          return function(e) {
+            var deviceToken;
+            deviceToken = e.deviceToken;
+            return _this.registerWithParse(e);
+          };
+        })(this),
+        error: function(e) {
+          Ti.API.debug('that was an error');
+          Ti.API.debug(e);
+          return alert(JSON.stringify(e));
+        }
+      });
     } else if (OS_ANDROID) {
       return alert('need to register for push on android');
     }
@@ -54,7 +50,7 @@ PushRegistration = {
     error.set('when', 'registering push notifications: registerForPush');
     error.save();
     alert("We had trouble registering your device for push notifications. We're looking into it.");
-    return Ti.API.info(JSON.stringify(e));
+    return alert(JSON.stringify(e));
   }
 };
 
--- src/lib/registerForPush.coffee
+++ src/lib/registerForPush.coffee
@@ -3,9 +3,9 @@ ParsePush = require 'parsePush'
 PushRegistration =
   subscribe: ->
     if OS_IOS
-      if Ti.Network.remoteNotificationsEnabled
-        Ti.API.info 'notifications are already enabled'
-      else
+      # if Ti.Network.remoteNotificationsEnabled
+      #   Ti.API.info 'notifications are already enabled'
+      # else
         Ti.Network.registerForPushNotifications
           types: [
             Ti.Network.NOTIFICATION_TYPE_ALERT,
@@ -47,7 +47,7 @@ PushRegistration =
     error.set 'when', 'registering push notifications: registerForPush'
     error.save()
     alert "We had trouble registering your device for push notifications. We're looking into it."
-    Ti.API.info JSON.stringify e
+    alert JSON.stringify e
 
 
 module.exports = PushRegistration
